6ae82adb7d20e9803c611e639a69a05d552dae36
Leila Abdelrahman <leila.zakaria@gmail.com>
2026-05-28 11:52:58 -0400

    [WIP] Implement the tokenizer class

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,7 +1,96 @@
 import regex as re
+from collections.abc import Iterable, Iterator
+import json 
 
 STOP_TOKEN = '<|endoftext|>'
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
+import regex as re
+from collections.abc import Iterable, Iterator
+import json 
+
+STOP_TOKEN = '<|endoftext|>'
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
+class Tokenizer:
+    def __init__(self, vocab: dict[int: bytes], merges, special_tokens=None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = sorted(special_tokens, key=len, reverse=True) if special_tokens is not None else []
+        self.bytes_to_id = {token_bytes: token_id for token_id, token_bytes in self.vocab.items()}
+
+        for token in self.special_tokens:
+            if token.encode('utf-8') not in self.vocab.values():
+                self.vocab[len(vocab)] = token.encode('utf-8')
+
+    @classmethod
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        vocab = {}
+        merges = []
+
+        with open(vocab_filepath, 'r') as file:
+            vocab_tmp = json.load(file)
+            vocab = {token_id : token_bytes_str.encode('utf-8') for token_bytes_str, token_id in vocab_tmp.items()}
+
+        with open(merges_filepath, 'r') as file:
+            merges = [tuple(line.split()) for line in file]
+
+        return cls(vocab, merges, special_tokens.sort(key=len))
+        
+    def encode(self, text: str) -> list[int]:
+        ids: [int] = []
+
+        if len(self.special_tokens) > 0:
+            pattern = '|'.join(map(re.escape, self.special_tokens))
+            chunks = re.split(f'({pattern})', text)
+        else:
+            chunks = [text]
+
+        all_encoded_pretokens = []
+
+        for chunk in chunks:
+            if chunk == '' or chunk is None: 
+                continue
+            if chunk in self.special_tokens:
+                ids.append(self.bytes_to_id[chunk.encode('utf-8')])
+                continue
+
+            pretokens = re.findall(PAT, chunk)
+
+            for pretoken in pretokens:
+                encoded_bytes = pretoken.encode('utf-8')
+                atoms = [bytes([b]) for b in encoded_bytes]
+                atoms = self.apply_merges(atoms, self.merges)
+                ids.extend([self.bytes_to_id[a] for a in atoms])
+        return ids
+
+    def apply_merges(self, atoms: list[bytes], merges: list[tuple]):
+        
+        for merge in merges:
+            new_atoms = []
+            i = 0
+
+            while i < len(atoms):
+                if i < len(atoms) - 1 and merge[0] == atoms[i] and merge[1] == atoms[i + 1]:
+                    new_atoms.append(atoms[i] + atoms[i + 1])
+                    i += 2
+                else:
+                    new_atoms.append(atoms[i])
+                    i += 1
+            atoms = new_atoms
+        return atoms
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        pass
+
+    def decode(self, ids: list[int]) -> str:
+        final_bytes = bytearray()
+        for id in ids:
+            final_bytes.extend(self.vocab[id])
+        return final_bytes.decode('utf-8', errors='replace')
+        
 
 def get_pretoken_counts(encoded_pretokens: list[tuple]):
     counts = {}
